9d052a9e2043b20d41e34c3d67720341a71dfa61
Crxshxr <103609586+Crxshxr@users.noreply.github.com>
2023-02-16 17:15:52 -0500

    :

---
+++ src/components/SecondGoal.js
@@ -0,0 +1,14 @@
+function SecondGoal() {
+    return (
+      <li>
+        <article>
+          <h2>Allow you to practice what you learned</h2>
+          <p>
+                "Im Just Better Lol :/"
+          </p>
+        </article>
+      </li>
+    );
+  }
+  
+  export default SecondGoal;
